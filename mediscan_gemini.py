c064298a61646dea15bf07fbb867f6145dd69d26
Hassaan-Raza <hassaanraza901@gmail.com>
2026-04-22 18:17:27 +0500

    Update mediscan_gemini.py
    change of models

--- mediscan_gemini.py
+++ mediscan_gemini.py
@@ -402,7 +402,7 @@ IMPORTANT INSTRUCTIONS:
         ]
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-3.1-flash-lite-preview",
         contents=contents
     )
     raw = response.text.strip()
